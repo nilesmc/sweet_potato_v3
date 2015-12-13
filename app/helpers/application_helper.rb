bf18db06c62a298e3fc49aa693d97833689038ef
Niles McGiver <niles@finedesigngroup.com>
2015-12-12 16:48:12 -0800

    updated application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def page_title
+    if @page_title.present?
+      @page_title
+    else
+      'Sweet Potato'
+    end
+  end
+
+  def body_class
+    "#{controller_name} #{action_name}"
+  end
+
 end
