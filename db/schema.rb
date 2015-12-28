84011dd0190f6e5782a95199a0462c92956ab0dd
Niles McGiver <niles@finedesigngroup.com>
2015-12-28 12:40:03 -0800

    added in coordinate columns to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213002421) do
+ActiveRecord::Schema.define(version: 20151228203857) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",           limit: 255
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20151213002421) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.string   "slug",            limit: 255
+    t.float    "latitude",        limit: 24
+    t.float    "longitude",       limit: 24
   end
 
   create_table "fae_changes", force: :cascade do |t|
