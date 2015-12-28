64858a54fb5d71fe13a8e58ba3abb85b5054640b
Niles McGiver <niles@finedesigngroup.com>
2015-12-28 13:14:08 -0800

    loading in assets neeed for google maps and geocoding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
 
   def home
+    @events = Event.all
+
+    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
+      marker.lat event.latitude
+      marker.lng event.longitude
+    end
   end
 
   def about
